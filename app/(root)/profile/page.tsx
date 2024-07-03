d0d468b9f8bdc4dcd6d0d894549ec9265abd7d6a
ej07delosreyes <amon26ej@gmail.com>
2024-07-04 00:55:20 +0800

    Initial Commit
    Done setting up convex need to build user.ts

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <div className='mt-9 flex flex-col gap-9'>
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-black-1">
+          Podcast Profile
+        </h1>
+      </section>
+    </div>
+  )
+}
+
+export default page
